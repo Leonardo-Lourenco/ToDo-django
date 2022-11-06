2fdbe9fb5282b815d378015cdebe1459346d90a5
Leonardo Lourenco <lnddru@gmail.com>
2022-11-06 17:11:23 -0300

    Add files via upload

--- tasks/forms.py
+++ tasks/forms.py
@@ -2,11 +2,19 @@
 # FORMULÁRIO PARA CADASTRAR AS TAREFAS
 # ESTE FORMULÁRIO IRÁ PEGAS OS DADOS DA NOSSA MODEL  , DENTRO DELA DELA A TASK
 
-from django.forms import ModelForm
+#from django.forms import ModelForm
+from django import forms
 from .models import Task
 
 
-class TaskForm(ModelForm):
+class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = ['title', 'describe', 'important'] # estes dados são da models , dentro da Task
+
+        #estilizando o fomulário do Template task_detalhe.html
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'describe': forms.Textarea(attrs={'class': 'form-control'}),
+            'important': forms.CheckboxInput(attrs={'class': 'form-check-input '}),
+        }
